1b7a65faa34b7f08dc37f8dba95e12fe747d0f60
Chiedo <chiedo@chie.do>
2014-11-04 13:54:24 -0500

    Made the companies page queryable

--- apps/company_directory/views.py
+++ apps/company_directory/views.py
@@ -4,7 +4,19 @@ from apps.company_directory.models import Company
 
 
 def index(request):
+    city = request.GET.get('city', '')
+    state = request.GET.get('state', '')
+    capital = request.GET.get('capital', '')
+
+    # get all of the companies then filter those companies based on the query made
     companies = Company.objects.all()
+    if(city != ''):
+        companies = companies.filter(city=city)
+    if(state != ''):
+        companies = companies.filter(state__name=state)
+    if(capital != ''):
+        companies = companies.filter(capital__gte=capital)
+
     return render(request, 'company_directory/index.html', {'companies': companies})
 
 
